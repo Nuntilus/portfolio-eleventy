77d4b831db398eb8a95b8debde5761d1225493fa
Nuntilus <153664386+Nuntilus@users.noreply.github.com>
2026-05-22 15:20:48 +0200

    initial commit
    initial commit
    
    initial commit

---
+++ src/data/hobbies.js
@@ -0,0 +1,7 @@
+module.exports = [
+  "Linux",
+  "Coding",
+  "Homelabing",
+  "Mountainbiking",
+]
+
---
+++ src/data/projects.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: "Nex-21 First steps" },
+  { name: "Gallileo" },
+  { name: "Appsteam" },
+  { name: "[M]ini-net" },
+];
+
