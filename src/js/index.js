17abb29d4fe6b9ad2bdcae5c03ead4ae5d545563
Nuntilus <153664386+Nuntilus@users.noreply.github.com>
2026-05-29 15:53:01 +0200

    feat: add cursor and scroll animation

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches)
+  addAnimation();
+
+function addAnimation() {
+  scrollers.forEach(scroller =>{
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach(item=>{
+      const dublicatedItems = item.cloneNode(true)
+      dublicatedItems.setAttribute('aria-hidden', true);
+      scrollerInner.appendChild(dublicatedItems);
+    })
+  })
+}
