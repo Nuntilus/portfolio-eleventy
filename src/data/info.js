99624735756201cf01b9f53ced21c165e649d8b6
Mio Galli <mio.galli@swisscom.com>
2026-06-04 14:32:05 +0200

    feat: add project pages

---
+++ src/data/info.js
@@ -0,0 +1,7 @@
+module.exports = [
+ { title: "Age", value: "10" },
+ { title: "Apprentice ship", value: "Software Developer" },
+ { title: "Apprentace-year", value: "2" },
+];
+
+
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,22 @@
 module.exports = [
-  { name: "Nex-21 First steps" },
-  { name: "Gallileo" },
-  { name: "Appsteam" },
-  { name: "[M]ini-net" },
+  {
+    name: "Nex-21 First steps",
+    slug: "nex-21-first-steps",
+    description: "A two week long entry into the buissnes world.",
+  },
+  {
+    name: "Gallileo",
+    slug: "gallileo",
+    description: "My first real Project i learned a lot of development skills like Angular and Java.",
+  },
+  {
+    name: "Appsteam",
+    slug: "appsteam",
+    description: "My second Project i learned a lot of typescritp and Nestjs. I also learned how to work together with other devs on biger Projects.",
+  },
+  {
+    name: "[M]ini-net",
+    slug: "mini-net",
+    description: "My next project still coming up",
+  },
 ];
-
