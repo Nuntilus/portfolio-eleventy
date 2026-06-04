0f128ac39f1fcc20b0507065321ac402f650bf83
Your Name <153664386+Nuntilus@users.noreply.github.com>
2026-06-04 14:52:12 +0200

    feat: added blog

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
 
+  eleventyConfig.addFilter("dateString", (date) => {
+    return new Date(date).toISOString().slice(0, 10);
+  });
+
   return {
     dir: {
       input: "src",
