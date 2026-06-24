f508e86e23b25e5676df93eaac207732dc860b78
Mio Galli <mio.galli@swisscom.com>
2026-06-24 15:02:10 +0200

    feat: add setup section

--- src/data/info.js
+++ src/data/info.js
@@ -2,6 +2,7 @@ module.exports = [
  { title: "Age", value: "16" },
  { title: "Apprentice ship", value: "Software Developer" },
  { title: "Apprentice-year", value: "2" },
+  {title: "Language", value: "DE, EN"}
 ];
 
 
