d6153543fa96bdb8e94e13137ad174c3f5f99115
Nuntilus <153664386+Nuntilus@users.noreply.github.com>
2026-06-24 15:02:10 +0200

    feat: add setup section

---
+++ src/data/setup.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { category: "OS",       tool: "Arch Linux" },
+  { category: "WM",       tool: "Hyprland"},
+  { category: "Terminal", tool: "Alacritty"},
+  { category: "Shell",    tool: "zsh" },
+  { category: "Editor",   tool: "Neovim" },
+  { category: "Hardware", tool: "ThinkPad P14s" },
+];
