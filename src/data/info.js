36ed5746fcf1102cf86dca72365709237b1aa666
Nuntilus <153664386+Nuntilus@users.noreply.github.com>
2026-06-26 11:12:38 +0200

    feat: updated all data

--- src/data/info.js
+++ src/data/info.js
@@ -1,8 +1,6 @@
 module.exports = [
- { title: "Age", value: "16" },
- { title: "Apprentice ship", value: "Software Developer" },
- { title: "Apprentice-year", value: "2" },
-  {title: "Language", value: "DE, EN"}
+  { title: "Age", value: "16" },
+  { title: "Apprentice ship", value: "Software Developer" },
+  { title: "Apprentice-year", value: "2" },
+  { title: "Language", value: "DE, EN" },
 ];
-
-
